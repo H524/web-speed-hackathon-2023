297bafe2726524fa278665af353c237186737c8a
hiroki-konishi <watasekoni6@gmail.com>
2023-03-04 15:35:19 +0900

    各Routerをlazy読み込みに変更

--- src/client/components/application/Routes/Routes.tsx
+++ src/client/components/application/Routes/Routes.tsx
@@ -1,11 +1,12 @@
-import type { FC } from 'react';
+import { FC, Suspense } from 'react';
 import * as Router from 'react-router-dom';
+import { lazyImport } from "../../../utils/load_lazy"
 
-import { NotFound } from '../../../pages/NotFound';
-import { Order } from '../../../pages/Order';
-import { OrderComplete } from '../../../pages/OrderComplete';
-import { ProductDetail } from '../../../pages/ProductDetail';
-import { Top } from '../../../pages/Top';
+const { NotFound } = lazyImport(() => import("../../../pages/NotFound"), 'NotFound');
+const { Order } = lazyImport(() => import("../../../pages/Order"), 'Order');
+const { OrderComplete } = lazyImport(() => import("../../../pages/OrderComplete"), 'OrderComplete');
+const { ProductDetail } = lazyImport(() => import("../../../pages/ProductDetail"), 'ProductDetail');
+const { Top } = lazyImport(() => import("../../../pages/Top"), 'Top');
 
 import { useScrollToTop } from './hooks';
 
@@ -13,12 +14,14 @@ export const Routes: FC = () => {
   useScrollToTop();
 
   return (
-    <Router.Routes>
-      <Router.Route element={<Top />} path="/" />
-      <Router.Route element={<ProductDetail />} path="/product/:productId" />
-      <Router.Route element={<Order />} path="/order" />
-      <Router.Route element={<OrderComplete />} path="/order/complete" />
-      <Router.Route element={<NotFound />} path="*" />
-    </Router.Routes>
+    <Suspense fallback={<>Loading...</>}>
+      <Router.Routes>
+        <Router.Route element={<Top />} path="/" />
+        <Router.Route element={<ProductDetail />} path="/product/:productId" />
+        <Router.Route element={<Order />} path="/order" />
+        <Router.Route element={<OrderComplete />} path="/order/complete" />
+        <Router.Route element={<NotFound />} path="*" />
+      </Router.Routes>
+    </Suspense>
   );
 };
---
+++ src/client/utils/load_lazy.ts
@@ -0,0 +1,14 @@
+import React, { ComponentType } from "react";
+
+export function lazyImport<
+  T extends { [P in U]: ComponentType },
+  U extends string,
+>(factory: () => Promise<T>, name: U): T {
+  return Object.create({
+    [name]: React.lazy(() => factory().then((module) => ({ default: module[name] }))),
+  });
+}
+
+// 参考URL
+// https://qiita.com/KokiSakano/items/b6d4e6875443064032b4
+// https://github.com/facebook/react/issues/14603#issuecomment-726551598
