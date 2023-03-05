eefaf7d41117d6ea8361cbb464481704459bee27
hiroki-konishi <watasekoni6@gmail.com>
2023-03-05 14:10:34 +0900

    mp4のサムネイル画像作成

--- src/client/components/product/ProductMediaListPreviewer/MediaItem/loadThumbnail.ts
+++ src/client/components/product/ProductMediaListPreviewer/MediaItem/loadThumbnail.ts
@@ -1,23 +1,24 @@
 export const loadThumbnail = async (url: string) => {
-  const video = document.createElement('video');
+  // const video = document.createElement('video');
 
-  await new Promise((resolve) => {
-    video.src = url;
-    video.addEventListener('canplaythrough', resolve, { once: true });
-  });
+  // await new Promise((resolve) => {
+  //   video.src = url;
+  //   video.addEventListener('canplaythrough', resolve, { once: true });
+  // });
 
-  const canvas = document.createElement('canvas');
-  const context = canvas.getContext('2d');
+  // const canvas = document.createElement('canvas');
+  // const context = canvas.getContext('2d');
 
-  canvas.width = video.videoWidth;
-  canvas.height = video.videoHeight;
-  if (context === null) {
-    return;
-  }
+  // canvas.width = video.videoWidth;
+  // canvas.height = video.videoHeight;
+  // if (context === null) {
+  //   return;
+  // }
 
-  video.currentTime = 0;
-  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+  // video.currentTime = 0;
+  // context.drawImage(video, 0, 0, canvas.width, canvas.height);
 
-  const thumbnailData = canvas.toDataURL('image/png');
-  return thumbnailData;
+  // const thumbnailData = canvas.toDataURL('image/png');
+  // return thumbnailData;
+  return url.replace(".mp4", ".webp")
 };
